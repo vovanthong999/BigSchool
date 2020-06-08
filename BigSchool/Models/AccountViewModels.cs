c6301b7e870e243ad44af43f314377c62e7cc3d7
Thong <tinhyeukhonoi123@gmail.com>
2020-06-08 11:23:51 +0700

    com

--- BigSchool/Models/AccountViewModels.cs
+++ BigSchool/Models/AccountViewModels.cs
@@ -79,7 +79,9 @@ namespace BigSchool.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
-        public string Name { get; internal set; }
+            [Required]
+            [StringLength(255)]
+        public string Name { get;  set; }
     }
 
     public class ResetPasswordViewModel
